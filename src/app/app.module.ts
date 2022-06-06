875aeee46ffef35f5b87664095be83563269d661
Patrick Walls <walls.patrick.a@gmail.com>
2022-06-05 23:51:55 -0500

    made some updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    HeaderComponent,
+    SearchComponent,
+    MoviesComponent,
+    TvShowsComponent
   ],
   imports: [
     BrowserModule,
